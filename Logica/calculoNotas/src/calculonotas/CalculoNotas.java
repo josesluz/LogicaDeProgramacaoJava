2f48d1211cc9e20783c46242e5e5726d96fb4718
José Luz <josesluz@gmail.com>
2018-11-28 18:35:26 -0300

    Interface Grafica para calculo de duas notas

--- Logica/calculoNotas/src/calculonotas/CalculoNotas.java
+++ Logica/calculoNotas/src/calculonotas/CalculoNotas.java
@@ -5,16 +5,16 @@ José Luz - josesluz@gmail.com
 
 */
 
-
 package calculonotas;
 
 import javax.swing.JOptionPane;
+import jdk.nashorn.internal.codegen.CompilerConstants;
 
-public class CalculoNotas {
-
+public class CalculoNotas extends operacoes{
+    
     
     public static void main(String[] args) {
-
+        
         //Solicita os atributos via JOptionPane para as variaveis xS e yS para realização do calculo.
         String xS = JOptionPane.showInputDialog(null, "Digite o valor de X :");
         String yS = JOptionPane.showInputDialog(null, "Digite o valor de Y :");
@@ -25,9 +25,22 @@ public class CalculoNotas {
 
         //Realiza calculos da média com os valores já recebido em double.
         double media = ((x+y)/2);
-
+        
         //Exibe a média via OPtionPane
         JOptionPane.showMessageDialog(null, "O valor do calculo é igual a: " +media);
-    }
-    
+        int opcao = JOptionPane.showOptionDialog(null, 
+            "Deseja realizar um novo calculo?", 
+            "Calculadora", 
+            JOptionPane.YES_NO_OPTION, 
+            JOptionPane.QUESTION_MESSAGE, 
+            null, null, null);
+            System.out.println();
+
+        // Interpretando a escolha do usuário, em uma próxima etapa serpa implementada a opção de fazer uma nova operação
+        if (opcao == JOptionPane.YES_OPTION){
+          JOptionPane.showMessageDialog(null, "A opção escolhida foi SIM");
+        } else {
+          JOptionPane.showMessageDialog(null, "A opção escolhida foi NÃO");
+        }
+    }   
 }
